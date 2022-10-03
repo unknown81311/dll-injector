908c2e22e7dd2e12e67e26253cd7334fff5b552b
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2022-10-02 22:37:05 -0600

    Update dll.cpp

--- dll.cpp
+++ dll.cpp
@@ -20,175 +20,174 @@
 #include <vector>
 #include <windows.h>
 #include <winuser.h>
+#include <cstdlib>
+#include <filesystem>
 
+namespace fsex = std::experimental::filesystem;
+namespace fs = std::filesystem;
 using namespace std;
 using std::string;
 
 int getPID() {
-	PROCESSENTRY32 entry;
-	entry.dwSize = sizeof(PROCESSENTRY32);
+  PROCESSENTRY32 entry;
+  entry.dwSize = sizeof(PROCESSENTRY32);
 
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
 
-	if (Process32First(snapshot, &entry) == TRUE) {
-		while (Process32Next(snapshot, &entry) == TRUE) {
-			if (stricmp(entry.szExeFile, "ScrapMechanic.exe") == 0) {
-				HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
-				CloseHandle(hProcess);
+  if (Process32First(snapshot, &entry) == TRUE) {
+    while (Process32Next(snapshot, &entry) == TRUE) {
+      if (stricmp(entry.szExeFile, "ScrapMechanic.exe") == 0) {
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
+        CloseHandle(hProcess);
 
-				return entry.th32ProcessID;
-			}
-		}
-	}
+        return entry.th32ProcessID;
+      }
+    }
+  }
 
-	CloseHandle(snapshot);
+  CloseHandle(snapshot);
 
-	return -1;
+  return -1;
 }
 
 std::vector<uint8_t> bypassDev(HANDLE con_handle) {
-	SignatureScanner sigScanner(L"ScrapMechanic.exe");
-	printf("HERE HERE HERE HERE HERE HERE\n");
-	std::cout << "HERE HERE HERE HERE HERE HERE\n";
+  SignatureScanner sigScanner(L"ScrapMechanic.exe");
 
-	constexpr size_t len = 9;
+  constexpr size_t len = 9;
 
-	if (!sigScanner.readMemory()) {
-		WriteConsole(con_handle, "failed to find dev flag\n", 21, NULL, NULL);
-		std::vector<uint8_t> signature_copy;
-		return signature_copy;
-	}
+  if (!sigScanner.readMemory()) {
+    WriteConsole(con_handle, "failed to find dev flag\n", 21, NULL, NULL);
+    std::vector<uint8_t> signature_copy;
+    return signature_copy;
+  }
 
-	DWORD64 compare_flag = sigScanner.scan("\x38\x43\x78\x0f\x85\xa2\x0a\x00\x00", "xxxxx????");
-	if (!compare_flag) {
-		WriteConsole(con_handle, "failed to find dev flag\n", 21, NULL, NULL);
-		std::vector<uint8_t> signature_copy;
-		return signature_copy;
-	}
+  DWORD64 compare_flag = sigScanner.scan("\x38\x43\x78\x0f\x85\xa2\x0a\x00\x00", "xxxxx????");
+  if (!compare_flag) {
+    WriteConsole(con_handle, "failed to find dev flag\n", 21, NULL, NULL);
+    std::vector<uint8_t> signature_copy;
+    return signature_copy;
+  }
 
-	string line = "found dev flag " + std::to_string(compare_flag) + '\n';
-	WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
+  string line = "found dev flag " + std::to_string(compare_flag) + '\n';
+  WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
 
-	LPVOID dst = (LPVOID) compare_flag;
-	DWORD oldProtection;
-	DWORD temp;
+  LPVOID dst = (LPVOID) compare_flag;
+  DWORD oldProtection;
+  DWORD temp;
 
-	// Allow modifications of the target function
-	VirtualProtect(dst, len, PAGE_EXECUTE_READWRITE, &oldProtection);
+  // Allow modifications of the target function
+  VirtualProtect(dst, len, PAGE_EXECUTE_READWRITE, &oldProtection);
 
-	std::vector<uint8_t> signature_copy((uint8_t *) compare_flag, (uint8_t *) compare_flag + len);
+  std::vector<uint8_t> signature_copy((uint8_t *) compare_flag, (uint8_t *) compare_flag + len);
 
-	// Replace the cmp and jne instructions with NOP
-	memset((void *) compare_flag, 0x90, len);
+  // Replace the cmp and jne instructions with NOP
+  memset((void *) compare_flag, 0x90, len);
 
-	// Restore protection
-	VirtualProtect(dst, len, oldProtection, &temp);
+  // Restore protection
+  VirtualProtect(dst, len, oldProtection, &temp);
 
-	return signature_copy;
+  return signature_copy;
 }
 
-// void loadSettings(HANDLE con_handle){
-//   DWORD processID = getPID();
+void loadSettings(HANDLE con_handle){
+  DWORD processID = getPID();
 
-//   HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
-//                           PROCESS_VM_READ,
-//                           FALSE, processID );
+  HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID );
 
-//   const char filePath[MAX_PATH];
-//   GetModuleFileNameExW(hProcess, NULL, filePath, MAX_PATH);
+  char filePath[MAX_PATH];
+  GetModuleFileNameExA(hProcess, NULL, (LPSTR) filePath, MAX_PATH);
 
-//   WriteConsole(con_handle, filePath, strlen(filePath), NULL, NULL);
+  WriteConsole(con_handle, filePath, strlen(filePath), NULL, NULL);
 
-//   fstream my_file;
-//   my_file.open("my_file", ios::out);
-//   if (!my_file) {
-//     cout << "File not created!";
-//   }
-//   else {
-//     cout << "File created successfully!";
-//     my_file.close();
-//   }
-// }
+  auto sm = fs::absolute(fs::path(filePath)).parent_path().parent_path();
+  sm /= "Sythe";
+  fs::create_directory(sm);
+  std::cout << sm << '\n';
+  sm /= "data";
+  std::cout << sm << '\n';
+  std::fstream data_file;
+  data_file.open(sm, std::fstream::out);
+}
 
 void writeUserData(HANDLE con_handle) {
-	SetConsoleTextAttribute(con_handle, FOREGROUND_RED);
+  SetConsoleTextAttribute(con_handle, FOREGROUND_RED);
 
-	char logo[] = "  ██████▓██   ██▓▄▄▄█████▓ ██░ ██ ▓█████ \n▒██    ▒ ▒██  ██▒▓  ██▒ ▓▒▓██░ ██▒▓█ "
-				  "  ▀ \n░ ▓██▄    ▒██ ██░▒ ▓██░ ▒░▒██▀▀██░▒███   \n  ▒   ██▒ ░ ▐██▓░░ ▓██▓ ░ ░▓█ "
-				  "░██ ▒▓█  ▄ \n▒██████▒▒ ░ ██▒▓░  ▒██▒ ░ ░▓█▒░██▓░▒████▒\n▒ ▒▓▒ ▒ ░  ██▒▒▒   ▒ ░░ "
-				  "   ▒ ░░▒░▒░░ ▒░ ░\n░ ░▒  ░ ░▓██ ░▒░     ░     ▒ ░▒░ ░ ░ ░  ░\n░  ░  ░  ▒ ▒ ░░   "
-				  " ░       ░  ░░ ░   ░   \n      ░  ░ ░               ░  ░  ░   ░  ░\n         ░ "
-				  "░                             \nInjected into ScrapMechanic.exe\n";
+  char logo[] = "  ██████▓██   ██▓▄▄▄█████▓ ██░ ██ ▓█████ \n▒██    ▒ ▒██  ██▒▓  ██▒ ▓▒▓██░ ██▒▓█ "
+          "  ▀ \n░ ▓██▄    ▒██ ██░▒ ▓██░ ▒░▒██▀▀██░▒███   \n  ▒   ██▒ ░ ▐██▓░░ ▓██▓ ░ ░▓█ "
+          "░██ ▒▓█  ▄ \n▒██████▒▒ ░ ██▒▓░  ▒██▒ ░ ░▓█▒░██▓░▒████▒\n▒ ▒▓▒ ▒ ░  ██▒▒▒   ▒ ░░ "
+          "   ▒ ░░▒░▒░░ ▒░ ░\n░ ░▒  ░ ░▓██ ░▒░     ░     ▒ ░▒░ ░ ░ ░  ░\n░  ░  ░  ▒ ▒ ░░   "
+          " ░       ░  ░░ ░   ░   \n      ░  ░ ░               ░  ░  ░   ░  ░\n         ░ "
+          "░                             \nInjected into ScrapMechanic.exe\n";
 
-	WriteConsole(con_handle, logo, strlen(logo), NULL, NULL);
+  WriteConsole(con_handle, logo, strlen(logo), NULL, NULL);
 
-	CSteamID steamID = SteamUser()->GetSteamID();
-	const char *steamNAME = SteamFriends()->GetPersonaName();
+  CSteamID steamID = SteamUser()->GetSteamID();
+  const char *steamNAME = SteamFriends()->GetPersonaName();
 
-	using std::string;
+  using std::string;
 
-	string line1 = "username: " + string(steamNAME) + '\n';
-	string line2 = "ID: " + std::to_string(steamID.ConvertToUint64()) + '\n';
+  string line1 = "username: " + string(steamNAME) + '\n';
+  string line2 = "ID: " + std::to_string(steamID.ConvertToUint64()) + '\n';
 
-	WriteConsole(con_handle, line1.c_str(), line1.size(), NULL, NULL);
-	WriteConsole(con_handle, line2.c_str(), line2.size(), NULL, NULL);
+  WriteConsole(con_handle, line1.c_str(), line1.size(), NULL, NULL);
+  WriteConsole(con_handle, line2.c_str(), line2.size(), NULL, NULL);
 }
 
 DWORD WINAPI Main(HMODULE hModule) {
-	const HANDLE con_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  const HANDLE con_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	// console logo and user data
-	writeUserData(con_handle);
+  // console logo and user data
+  writeUserData(con_handle);
 
-	// load settings
-	// loadSettings(con_handle);
+  // load settings
+  loadSettings(con_handle);
 
-	// dev bypass
-	std::vector<uint8_t> signature = bypassDev(con_handle);
-	std::fflush(stdout);
-	std::cout << std::flush;
+  // dev bypass
+  std::vector<uint8_t> signature = bypassDev(con_handle);
+  std::fflush(stdout);
+  std::cout << std::flush;
 
-	if (signature.size() == 0) {
-		std::cout << "An error occurred in bypassDev\n";
-	} else {
-		string line = "Signature: ";
+  if (signature.size() == 0) {
+    std::cout << "An error occurred in bypassDev\n";
+  } else {
+    string line = "Signature: ";
 
-		for (size_t i = 0; i < signature.size(); ++i) {
-			line += std::to_string(signature[i]);
-			line += ' ';
-		}
+    for (size_t i = 0; i < signature.size(); ++i) {
+      line += std::to_string(signature[i]);
+      line += ' ';
+    }
 
-		line += '\n';
-		WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
-	}
+    line += '\n';
+    WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
+  }
 
-	// getkeys
-	while (1) {
-		for(int i = VK_LBUTTON; i < VK_OEM_CLEAR;i++){
-			if (GetAsyncKeyState(VK_TAB) & 0x01) {
-				WriteConsole(con_handle, "pressed TAB\n", 13, NULL, NULL);
-			}
-			if (GetAsyncKeyState(i) & 0x01) {
-				string line = "pressed: " + std::to_string(i) + "\n";
-				WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
-			}
-		}
-	}
-	return 0;
+  // getkeys
+  while (1) {
+    for(int i = VK_LBUTTON; i < VK_OEM_CLEAR;i++){
+      if (GetAsyncKeyState(VK_TAB) & 0x01) {
+        WriteConsole(con_handle, "pressed TAB\n", 13, NULL, NULL);
+      }
+      if (GetAsyncKeyState(i) & 0x01) {
+        string line = "pressed: " + std::to_string(i) + "\n";
+        WriteConsole(con_handle, line.c_str(), line.size(), NULL, NULL);
+      }
+    }
+  }
+  return 0;
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved) {
-	switch (fdwReason) {
-		case DLL_PROCESS_ATTACH: {
-			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) Main, hModule, NULL, NULL);
-			break;
-		}
-		case DLL_PROCESS_DETACH:
-			break;
-		case DLL_THREAD_ATTACH:
-			break;
-		case DLL_THREAD_DETACH:
-			break;
-	}
-	return TRUE;
+  switch (fdwReason) {
+    case DLL_PROCESS_ATTACH: {
+      CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) Main, hModule, NULL, NULL);
+      break;
+    }
+    case DLL_PROCESS_DETACH:
+      break;
+    case DLL_THREAD_ATTACH:
+      break;
+    case DLL_THREAD_DETACH:
+      break;
+  }
+  return TRUE;
 }
